556b98bc3d2f1605edea0a4143fa6708a1b9c2db
Verita' Michele <verita.michele@osdbsesto>
2020-11-19 13:40:33 +0100

    edit dom with jquery

--- es2.js
+++ es2.js
@@ -1,4 +1,4 @@
-var addBtn = function(){
+/*var addBtn = function(){
     var btn = document.createElement("button");
     btn.innerHTML = "NON CLICCARE";
   
@@ -9,12 +9,10 @@ var addBtn = function(){
   
   var changeColor = function () {
     console.log("BUONGIORNISSIMOOO!!1!!1");
-    document
-      .getElementById("div1")
-      .setAttribute("style", "background-color:   rgb(163, 36, 68)");
-    document.getElementById("span1").innerHTML = "CUCU";
-    document.getElementById("div1").innerHTML =
-      "<h1>TI AVEVO DETTO DI NON CLICCARE MALEDETTO</h1>";
+    document.getElementById("div1").setAttribute("style", "background-color:   rgb(163, 36, 68)");
+    document.getElementById("span1").innerHTML ="CUCU";
+    document.getElementById("div1").innerHTML ="<h1>TI AVEVO DETTO DI NON CLICCARE MALEDETTO</h1>";
   };
   
   document.addEventListener("DOMContentLoaded", addBtn);
+*/
